8bf400923600d41d3d5689dc4aa2c152bc38a069
emilyaand <104448537+emilyaand@users.noreply.github.com>
2022-05-19 15:53:32 +0400

    Add files via upload

---
+++ Workdesk/Workdesk/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Emilya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 9 };
+
+            Console.WriteLine("Enter Age ");
+            string ageStr = Console.ReadLine();
+            int age = int.Parse(ageStr);
+
+            bool flag = false;
+
+            foreach (int element in arr)
+            {
+
+                if (element == age)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            Console.WriteLine(flag);
+
+        }
+    }
+}
